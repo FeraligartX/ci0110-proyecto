423d01088738411c67e786a495c8ca7a05341629
Maikol Bermúdez <MBERM@Maikol.com>
2026-05-08 12:46:59 -0600

    Versión funcional del tablero

---
+++ proyecto_naval.py
@@ -0,0 +1,33 @@
+def crearTableroVacio():
+    print("entro a la fn crearTableroVacio")
+    matriz = [[ '~' for i in range(8)] for j in range(8)]
+    cadena = ""
+    for i in range(0,8):
+        cadena+=' | '
+        for j in range(8):
+            cadena+= matriz[i][j] +' ' + '|' + ' ' 
+        cadena += '\n'
+    print(cadena)
+    print("salió de la fn crearTableroVacio")
+    return matriz
+def mostrarTablero():
+    print("entro a la fn mostrarTablero")
+def colocarBarco():
+    print("entro a la fn colocarBarcoo")
+def validarUbicacion():
+    print("entro a la fn validarUbicacion")
+def disparar():
+    print("entro a la fn disparar")
+def iniciarPartida():
+    print("entro a la fn iniciarPartida")
+def mostrarEstadísticas():
+    print("entro a la fn mostrarEstadísticas")
+def mostrarAyuda():
+    print("entro a la fn mostrarAyuda")
+def mostrarAcercaDe():
+    print("entro a la mostrarAcercaDe")
+    
+if __name__ == '__main__':
+    tablero1 = crearTableroVacio()
+    tableroComp = crearTableroVacio()
+    
