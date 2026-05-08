6c42d6340552c22f2f7c37a37c1f70e9f0374bba
Maikol Bermúdez <MBERM@Maikol.com>
2026-05-08 09:00:20 -0600

    Primer commit

---
+++ proyecto naval.py
@@ -0,0 +1,31 @@
+def crearTableroVacio():
+    print("entro a la fn crearTableroVacio")
+    matriz = [[ '~' for i in range(8)] for j in range(8)
+    for i in range(0,8):
+        cadena +=' | '
+        for j in range(8):
+            cadena+= matriz[i][i] +' ' + '|' + ' ' 
+        cadena += '\n'
+    print(cadena)
+    print("salió de la fn crearTableroVacio")
+    return matriz
+def mostrarTablero():
+    print("entro a la fn mostrarTablero")
+def colocarBarco():
+    print("entro a la fn colocarBarcoo")
+def validarUbicacion():
+    print("entro a la fn validarUbicacion")
+def disparar():
+    print("entro a la fn disparar")
+def iniciarPartida():
+    print("entro a la fn iniciarPartida")
+def mostrarEstadísticas():
+    print("entro a la fn mostrarEstadísticas")
+def mostrarAyuda():
+    print("entro a la fn mostrarAyuda")
+def mostrarAcercaDe():
+    print("entro a la mostrarAcercaDe")
+    
+if _name_ == '_name_':
+    tablero1 = crearTableroVacio()
+    tableroComp = 
